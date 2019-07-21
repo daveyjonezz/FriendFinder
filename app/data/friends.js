6078f46c8b6b1186d2d70b419a2c108821e8aa8c
Davis Birsan <davis.birsan@gmail.com>
2019-07-20 22:43:45 -0500

    everything but looping

---
+++ app/data/friends.js
@@ -0,0 +1,10 @@
+var friendArray = [
+    {
+      Name: "Davis Birsan",
+      Image: "https://media.licdn.com/dms/image/C4E03AQHqkacyIAqEjg/profile-displayphoto-shrink_200_200/0?e=1569456000&v=beta&t=qPZ6gTnSOtlQMoCU2Wkw6_PBCZ338xktIwm0O4xE6e4",
+      Scores: [3, 5, 5, 3, 5, 5, 5, 5, 5, 5]
+    }
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = friendArray;
---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var app = express();
+var PORT = process.env.PORT || 8080;
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static(__dirname + '/app/public'));
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+app.listen(PORT, function(){
+    console.log("App listing on http://localhost:"+PORT)
+})
