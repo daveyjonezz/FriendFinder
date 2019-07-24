198e9ca1f39695d9d0861bfaae7e0610106ef4a6
Davis Birsan <davis.birsan@gmail.com>
2019-07-23 21:39:57 -0500

    updated stuff

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,34 @@
 var friendArray = [
     {
-      Name: "Davis Birsan",
-      Image: "https://media.licdn.com/dms/image/C4E03AQHqkacyIAqEjg/profile-displayphoto-shrink_200_200/0?e=1569456000&v=beta&t=qPZ6gTnSOtlQMoCU2Wkw6_PBCZ338xktIwm0O4xE6e4",
-      Scores: [3, 5, 5, 3, 5, 5, 5, 5, 5, 5]
-    }
+      name: "Steven Hyde",
+      photo: "https://d1o51r9qdgnnlz.cloudfront.net/static_image/6d38a621-4d1e-44b7-b20f-c67a98ff82f4/static.jpg",
+      scores: [3, 5, 5, 3, 5, 5, 5, 5, 5, 5]
+    },
+    {
+      name: "Jackie Burkhart",
+      photo: "https://66.media.tumblr.com/4e9d648ea1ddc00c6169ed3d53d4b879/tumblr_p8mfouA9yp1w5jef7o5_250.png",
+      scores: [5, 1, 2, 3, 1, 4, 5, 2, 1, 4]
+    },
+    {
+      name: "Donna Pinciotti",
+      photo: "https://66.media.tumblr.com/6e471ca337e7834d29f7369543d0b414/tumblr_pio2fmi6Z01tdxyhao2_250.png",
+      scores: [5, 5, 1, 5, 3, 5, 3, 3, 1, 3]
+    },
+    {
+      name: "Fez",
+      photo: "https://i.pinimg.com/originals/86/d4/58/86d45827463a91d4e0d5cf09d3bff515.jpg",
+      scores: [5, 5, 3, 1, 3, 5, 3, 5, 5, 2]
+    },
+    {
+      name: "Michael Kelso",
+      photo: "https://vignette.wikia.nocookie.net/the-70s/images/f/fb/Kelso.jpg/revision/latest?cb=20110327215340",
+      scores: [5, 3, 3, 3, 3, 3, 2, 4, 5, 4]
+    },
+    {
+      name: "Eric Forman",
+      photo: "https://vignette.wikia.nocookie.net/that70sshow/images/0/0c/Eric.jpg/revision/latest/top-crop/width/240/height/240?cb=20160617210320",
+      scores: [1, 1, 2, 2, 1, 1, 1, 2, 5, 1]
+    },
   ];
   
   // Note how we export the array. This makes it accessible to other files using require.
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -24,12 +24,68 @@ module.exports = function(app) {
   // (ex. User fills out a reservation request... this data is then sent to the server...
   // Then the server saves the data to the tableData array)
   // ---------------------------------------------------------------------------
-  app.post("/api/friends", function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body parsing middleware
-      friendData.push(req.body);
-      res.json(true);
-  });
+  // app.post("/api/friends", function(req, res) {
+  //   // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+  //   // It will do this by sending out the value "true" have a table
+  //   // req.body is available since we're using the body parsing middleware
+  //     friendData.push(req.body);
+  //     res.json(true);
+  // });
   // ---------------------------------------------------------------------------
+  app.post("/api/friends", function(req,res){
+      
+    var userInputMessageBodyObject=req.body;
+
+   console.log('LOOK', userInputMessageBodyObject)
+
+    //store the total difference
+    var totalDifference=0;
+
+    //store the result for each substraction from the scores
+    var result=0;
+
+    //make a new array to store another array of obects with
+    //total difference in the scores
+    var newTablewithTotalDifference =[]
+    
+
+    //outer for loop to iterate the entire array of object from table
+    for(var i=0; i< friendData.length;i++){
+
+        //reset the total difference for the next object person
+        totalDifference=0;
+
+        //inner for loop to calculate the new Person from the user Input off req.body
+        for(var j=0; j< userInputMessageBodyObject.scores.length;j++){
+
+            //subtract each object score from first element to the last element of the array
+             result=  parseFloat(friendData[i].scores[j]) - parseFloat(userInputMessageBodyObject.scores[j])
+        
+             //sum up the subtraction and stores in totatdifference
+            //and change the values to be positive
+             totalDifference += Math.abs(result)
+         }
+        
+        //inserts into a new array of object for sorting the least total difference
+         newTablewithTotalDifference.push({
+             friendData: friendData[i].name,
+             totalDifference: totalDifference,
+             photo: friendData[i].photo
+         })
+    }  
+
+  //add a new person to our table from 
+    //userInput that is stored in req.body
+    friendData.push(userInputMessageBodyObject);
+ 
+    //sorts the new array of object against the total difference
+    newTablewithTotalDifference.sort(function(a, b){
+        return a.totalDifference-b.totalDifference
+    })
+
+    console.log(newTablewithTotalDifference)
+    
+    //returns first element and response back the total least difference to the           //client during the post request 
+    res.json(newTablewithTotalDifference[0]);
+})
 };
